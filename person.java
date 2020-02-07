ebc497033807428fe40b175b910236f6c1da1d01
Mohamed Jama <mohamed.jama@ucdconnect.ie>
2020-02-07 21:15:43 +0000

    Create person.java

---
+++ person.java
@@ -0,0 +1,28 @@
+interface Person{
+  public String getFullName();
+  public void setFirstName(String firstname);
+  public void setSurname(String surname);
+  public String getSurname();
+  public String getFirstname();
+  
+  public void setID(int id);
+  public int getID();
+  
+  public String getGender();
+  public void setGender(String gender);
+  
+  public String getNationality();
+  public void setNationality(String nationality);
+  
+  public int getAge();
+  public void setAge(int age);
+  
+  public String getAddress();
+  public void setAddress(String address);
+  
+  public String getEmail();
+  public void setEmail(String email);
+  
+  public String getPhoneNumber();
+  public void setPhoneNumber(String phoneNumber);
+}
